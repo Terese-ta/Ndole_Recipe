143005f1cd4c6c45f055e1c8d8a3a8752ce499eb
Terese-ta <terese.akuma@yahoo.ca>
2021-10-23 14:21:28 -0400

    added recipe in test.java

---
+++ test.java
@@ -0,0 +1,49 @@
+TERESE'S NDOLE RECIPE
+
+NDOLE
+Ndole is a Cameroonian dish made with bitter leaves and raw peanuts.
+
+This is a creamy groundut paste, with meat and a combination
+or meat and fish, fried in vegetable oil.
+
+This meal is a delicacy in Cameroon, served with a choice of plantains, 
+yam, garri or bobolo. It is great to feed a large family at lunch or dinner.
+
+
+Ingredients:
+
+3 packs of washed Bitter leaf 
+1 bag raw peeled groundnuts
+Cow trotters (Cow leg)
+Dried Fish
+Beef
+Vegetable oil
+Chicken/Poulet broth mix (maggi)
+Salt to taste
+Crayfish
+Pepper 
+Garlic
+
+
+How to Make Ndole
+
+1) Pour water in a deep pot, preerably stainless still till it boils
+2) Add the fresh packs of ndole and stir to soten the ndole 
+3) Add a pinch of baking soda or limestone for softening
+4) Rinse ndole and squeeze water out with the hands till ndole in small tennis balls
+5) Boil the peeled peanuts with water and a pinch of salt for about five minutes
+6) Blend the groundnuts in a blender, with garlic and pepper
+7) Boil bee with desired seasoning to taste
+8) Boil cow trotters for about an hour or 20 minutes in a power cooker
+9) Heat vegetable oil in a deep pot
+10) Fry the beef briefly for about 5 mins
+11) Add the ndole to the oil and meat and fry for another 10 mins on low heat
+12) Add the ndole to the pot of groundnut paste and stir
+13) Add the cow trotters
+14) Add beef or chicken broth
+15) Add salt to your desired taste
+16) Add ground crayfish 
+17) ry everything or a ew minutes
+18) In a separate pot, fry the sliced onion or 2-3 mins to make the onion tender
+19) Pour the onion and oil in the pot of ndole containing the rest o the ingredients
+20) Serve ndole with either ried plantain, boiled plantain or boiled bobolo
